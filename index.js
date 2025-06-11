69d3da6d2f0f7d36de488db884f01f9b1b2921de
Godson Sojan <godson.sojan@nestgroup.net>
2025-06-12 00:15:33 +0530

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors())
+
+app.post('/api/User/register', (req, res) => {
+    console.log(req.body)
+    res.json({
+        "success": true,
+    });
+});
+
+app.get('/', (req, res) => {
+    res.json({ "sucess": true, "message": "Welcome to the API" });
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
